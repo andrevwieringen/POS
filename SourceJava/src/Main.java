57d4b72e21f8b6ef1caa44674b11bb7e01044481
andrevwieringen <andrevanwieringen@macbook-pro-3.lan>
2024-01-29 11:28:39 +0100

    POS2

--- SourceJava/src/Main.java
+++ SourceJava/src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+
+    static public void main(String args[]){
+        ProductCatalog catalog = new ProductCatalog();
+        Inventory inventory = new Inventory();
+        Accounting accounting = new Accounting();
+        PaymentTerminal terminal = new PaymentTerminal(catalog,inventory,accounting);
+    }
 }
--- SourceJava/src/PaymentTerminal.java
+++ SourceJava/src/PaymentTerminal.java
@@ -1,2 +1,12 @@
 public class PaymentTerminal {
+
+    private ProductCatalog catalog;
+    private Inventory inventory;
+    private Accounting accounting;
+
+    public PaymentTerminal(ProductCatalog catalog,Inventory inventory, Accounting accounting){
+        this.catalog = catalog;
+        this.inventory = inventory;
+        this.accounting = accounting;
+    }
 }
